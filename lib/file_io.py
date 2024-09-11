845e662b1fe79ada5c6482665e735c80c51ee3cb
eustachekamala <eustachejared25@gmail.com>
2024-09-11 12:51:28 +0300

    I created some functions to read, create, and open the files

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt','a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as f:
+        return f.read()
+
+    
+
+## TESTS
+if __name__ == '__main__':
+    write_file('test.txt', 'Hello World!')
+    print(read_file('test.txt'))
+    append_file('test.txt', ' and goodbye!')
+    print(read_file('test.txt'))
